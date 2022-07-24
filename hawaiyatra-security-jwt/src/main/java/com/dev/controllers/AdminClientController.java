c91a474272ff2d2bbbbd22900b96be3c8f8127fe
cogjava1794 <cogjava1794@ctsjava723.iiht.tech>
2022-07-25 01:53:44 +0530

    commit

--- hawaiyatra-security-jwt/src/main/java/com/dev/controllers/AdminClientController.java
+++ hawaiyatra-security-jwt/src/main/java/com/dev/controllers/AdminClientController.java
@@ -3,6 +3,7 @@ package com.dev.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,6 +16,7 @@ import com.dev.client.AdminClient;
 import com.dev.models.FlightDetails;
 import com.dev.models.StatusUpdate;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/service")
 public class AdminClientController {
